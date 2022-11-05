3124225b76f09ee071c746c18a715674c3b7a1ae
alexanderono7 <alexanderono7@gmail.com>
2022-11-04 23:57:57 -0700

    can iterate through processes and find the corresponding PID

--- memory_manager.c
+++ memory_manager.c
@@ -1,13 +1,29 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
-// Receive arguments to the kernel module
 static int pid = 0;
+// Receive arguments to the kernel module
 module_param(pid, int, 0644);
+
+int find_pid(void){
+    struct task_struct* p;
+    
+    for_each_process(p){
+        if(p->pid == pid){
+            printk("\nFOUND IT! : %d\n",p->pid);
+        }
+        printk("don't care: %d",p->pid);
+    }
+    return 0;
+}
 
 // Initialize kernel module
 int memman_init(void){
     printk("Memory manager launched!\n");
+    find_pid();
     return 0;
 }
 
