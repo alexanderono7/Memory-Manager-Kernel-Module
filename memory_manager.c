d4ca06ad538f0cb9612435b669d8cb4e61811d17
alexanderono7 <alexanderono7@gmail.com>
2022-11-05 13:21:39 -0700

    I think prep test and clear function is working.

--- memory_manager.c
+++ memory_manager.c
@@ -5,22 +5,40 @@
 #include <linux/sched.h>
 #include <linux/sched/signal.h>
 #include <linux/mm.h>
+#include <linux/hrtimer.h>
 
 static int pid = 0;
 static int rss_pages = 0;
 static int swap_pages = 0;
 
+int ptep_test_and_clear_young (struct vm_area_struct *vma, unsigned long addr, pte_t *ptep);
+/* Test and clear the accessed bit of a given pte entry. vma is the pointer
+to the memory region, addr is the address of the page, and ptep is a pointer
+to a pte. It returns 1 if the pte was accessed, or 0 if not accessed. */
+/* The ptep_test_and_clear_young() is architecture dependent and is not
+exported to be used in a kernel module. You will need to add its
+implementation as follows to your kernel module. */
+int ptep_test_and_clear_young(struct vm_area_struct *vma,
+unsigned long addr, pte_t *ptep)
+{
+    int ret = 0;
+    if (pte_young(*ptep)){
+        ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
+    }
+    return ret;
+}
+
 // Receive arguments to the kernel module
 module_param(pid, int, 0644);
 
 // experimental function - access a page? idk.
-void access_page(struct mm_struct* mm, unsigned long address){
+pte_t* access_page(struct mm_struct* mm, unsigned long address){
     pgd_t *pgd;
     p4d_t *p4d;
     pmd_t *pmd;
     pud_t *pud;
-    pte_t *ptep;
-    //pte_t *ptep, pte;
+    pte_t *ptep, pte;
+    pte_t *result = NULL;
 
     swap_pages++;
 
@@ -28,7 +46,7 @@ void access_page(struct mm_struct* mm, unsigned long address){
     if (pgd_none(*pgd) || pgd_bad(*pgd)) {
         // check if pgd is bad or does not exist
         //printk("pgd is bad or does not exist");
-        return;
+        return NULL;
     }else{
         //printk("pgd is good!");
     }
@@ -37,7 +55,7 @@ void access_page(struct mm_struct* mm, unsigned long address){
     if (p4d_none(*p4d) || p4d_bad(*p4d)) {
         // check if p4d is bad or does not exist
         //printk("p4d is bad or does not exist");
-        return;
+        return NULL;
     }else{
         //printk("p4d is good!");
     }
@@ -47,6 +65,7 @@ void access_page(struct mm_struct* mm, unsigned long address){
         // check if pud is bad or does not exist
         //printk("pud is bad or does not exist");
         //return;
+        return NULL;
     }else{
         //printk("pud is good!");
     }
@@ -55,7 +74,7 @@ void access_page(struct mm_struct* mm, unsigned long address){
     if (pmd_none(*pmd) || pmd_bad(*pmd)) {
         // check if pmd is bad or does not exist
         //printk("pmd is bad or does not exist");
-        return;
+        return NULL;
     }else{
         //printk("pmd is good!");
     }
@@ -64,15 +83,16 @@ void access_page(struct mm_struct* mm, unsigned long address){
     if (!ptep){
         // check if pte does not exist
         //printk("pte is bad or does not exist");
-        return;
+        return NULL;
     }else{
         //printk("pte is good!");
     }
 
-    //pte = *ptep; //is this necessary??
+    pte = *ptep; //is this necessary??
+    result = ptep;
     rss_pages++;
     swap_pages--;
-    return;
+    return result;
 }
 
 // Print out all tasks which have a valid mm and mmap (used for testing purposes)
@@ -105,19 +125,24 @@ void probe(void){
                         access_page(p->mm, i);
                         //if(i==end) printk("completed.");
                     }
+                    int res;
+                    pte_t *ptep;
                     while(foo->vm_next){
                         start = foo->vm_start;
                         end = foo->vm_end;
                         for(i = start; i <= end; i+=PAGE_SIZE){
-                            access_page(p->mm, i);
-                            //if(i==end) printk("completed.");
+                            ptep = access_page(p->mm, i);
+                            if(ptep){
+                                res = ptep_test_and_clear_young(foo, i, ptep);
+                                printk("ptep test value: %d", res);
+                            }
                         }
                         foo = foo->vm_next;
                     }
                 }
             }
         }
-        //return; // I only want to do one process for testing the page walk.
+        return; // I only want to do one process for testing the page walk.
     }
     return;
 }
@@ -173,11 +198,28 @@ int traverse_vmas(struct task_struct* task){
     return 0;
 }
 
+// 10 second timer for measuring 1 instance of the WSS
+unsigned long timer_interval_ns = 10e9; // 10-second timer
+enum hrtimer_restart no_restart_callback(struct hrtimer *timer){
+    ktime_t currtime, interval;
+    currtime = ktime_get();
+    interval = ktime_set(0, timer_interval_ns);
+    hrtimer_forward(timer, currtime, interval);
+    // Do the measurement
+    printk("Timer...running?");
+    return HRTIMER_NORESTART;
+}
+
+// Count # of page table entries being accessed for a given process right now
+//int count_ptes(){ }
+
+
 // Initialize kernel module
 int memman_init(void){
     struct task_struct* proc;
     //printk("Memory manager launched!\n");
-    //probe();
+    probe();
+    return 0;
     proc = find_pid();
     if(!proc){
         printk("Couldn't find process w/ PID %d", pid);
@@ -186,6 +228,7 @@ int memman_init(void){
     traverse_vmas(proc);
     printk("pages in rss: %d", rss_pages);
     printk("pages in swap: %d", swap_pages);
+
     return 0;
 }
 
