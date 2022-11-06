b2bc8d19439bfe3287cb630b7710bba446ed98d8
alexanderono7 <alexanderono7@gmail.com>
2022-11-06 01:44:50 -0700

    might change timer again

--- memory_manager.c
+++ memory_manager.c
@@ -7,8 +7,8 @@
 #include <linux/hrtimer.h>
 #include <linux/ktime.h>
 
-#define TIMEOUT_NSEC   ( 0 )      //1 second in nano seconds
-#define TIMEOUT_SEC    ( 10 )                //10 seconds (?)
+#define TIMEOUT_NSEC   ( 1000000000L )      //1 second in nano seconds
+#define TIMEOUT_SEC    ( 9 )                //10 seconds (?)
 
 static int pid = 0;
 static unsigned int rss_pages = 0;
