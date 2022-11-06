7702382c06b496bd2bcc4f1b025bfae587c5755c
alexanderono7 <alexanderono7@gmail.com>
2022-11-05 19:58:11 -0700

    testing if pte read/write perms have any impact

--- memory_manager.c
+++ memory_manager.c
@@ -26,6 +26,9 @@ exported to be used in a kernel module. You will need to add its
 implementation as follows to your kernel module. */
 int ptep_test_and_clear_young(struct vm_area_struct *vma, unsigned long addr, pte_t *ptep) {
     int ret = 0;
+    if(!pte_read(*ptep) or !pte_mkwrite(*ptep)){
+        return ret;
+    }
     if(pte_present(*ptep)){
         rss_pages++;
     }else{
