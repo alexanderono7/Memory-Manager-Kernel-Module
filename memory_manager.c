63ab7894e1bdc8cce15ba4a54bdf40e6696fa0b1
alexanderono7 <alexanderono7@gmail.com>
2022-11-06 19:15:25 -0700

    Nearing final

--- memory_manager.c
+++ memory_manager.c
@@ -19,88 +19,43 @@ static unsigned int swap_pages = 0;
 static unsigned int wss_pages = 0;
 struct task_struct* process;
 static int counter = 0;
-
-/* Test and clear the accessed bit of a given pte entry. vma is the pointer
-to the memory region, addr is the address of the page, and ptep is a pointer
-to a pte. It returns 1 if the pte was accessed, or 0 if not accessed. */
-
-/* The ptep_test_and_clear_young() is architecture dependent and is not
-exported to be used in a kernel module. You will need to add its
-implementation as follows to your kernel module. */
-int ptep_test_and_clear_young(struct vm_area_struct *vma, unsigned long addr, pte_t *ptep) {
-    int ret = 0;
-    if (pte_young(*ptep)){
-      ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte); //returns 1 if pte accessed
-    }
-
-    return ret;
-}
+static struct hrtimer etx_hr_timer;
+ktime_t ktime;
 
 // Receive arguments to the kernel module
 module_param(pid, int, 0644);
 
-void experimental(struct mm_struct* mm, unsigned long address){
-    pgd_t *pgd;
-    p4d_t *p4d;
-    pmd_t *pmd;
-    pud_t *pud;
-    pte_t *ptep, pte;
-
-    pgd = pgd_offset(mm, address); // get pgd from mm and the page address
-    //if (pgd_none(*pgd)) return;
-
-    p4d = p4d_offset(pgd, address); //get p4d from from pgd and the page address
-    //if (p4d_none(*p4d)) return;
-
-    pud = pud_offset(p4d, address); // get pud from from p4d and the page address
-    //if (pud_none(*pud)) return;
-
-    pmd = pmd_offset(pud, address); // get pmd from from pud and the page address
-    //if (pmd_none(*pmd)) return;
-
-    ptep = pte_offset_map(pmd, address); // get pte from pmd and the page address
-    //if (!ptep) return;
-    if(!pte_present(*ptep)){
-        swap_pages++;
-    }
-}
-
 // "Page table walk" - given mm and addr, walk to its page table entry (quit midway if it isn't valid).
-pte_t* access_page(struct mm_struct* mm, unsigned long address){
+void access_page(struct mm_struct* mm, unsigned long address){
     pgd_t *pgd;
     p4d_t *p4d;
     pmd_t *pmd;
     pud_t *pud;
-    pte_t *ptep, pte;
-    pte_t *result = NULL;
+    pte_t *ptep;
 
     pgd = pgd_offset(mm, address); // get pgd from mm and the page address
-    if (pgd_none(*pgd) || pgd_bad(*pgd)) return NULL;
+    if (pgd_none(*pgd) || pgd_bad(*pgd)) return;
 
     p4d = p4d_offset(pgd, address); //get p4d from from pgd and the page address
-    if (p4d_none(*p4d) || p4d_bad(*p4d)) return NULL;
+    if (p4d_none(*p4d) || p4d_bad(*p4d)) return;
 
     pud = pud_offset(p4d, address); // get pud from from p4d and the page address
-    if (pud_none(*pud) || pud_bad(*pud)) return NULL;
+    if (pud_none(*pud) || pud_bad(*pud)) return;
 
     pmd = pmd_offset(pud, address); // get pmd from from pud and the page address
-    if (pmd_none(*pmd) || pmd_bad(*pmd)) return NULL;
+    if (pmd_none(*pmd) || pmd_bad(*pmd)) return;
 
     ptep = pte_offset_map(pmd, address); // get pte from pmd and the page address
-    //wss_pages += ptep_test_and_clear_young(mm->mmap, address, ptep);
     if (pte_young(*ptep)) wss_pages += test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
-    if (!ptep) return NULL;
+    if (!ptep) return;
 
-    pte = *ptep;
-    experimental(mm, address);
     if(!pte_none(*ptep)){
         if(pte_present(*ptep)){ 
             rss_pages++;
         }else{
-            //swap_pages++;
+            swap_pages++;
         }
     }
-    return result;
 }
 
 // Find the process whose PID matches `pid` and return its task_struct ptr
@@ -154,8 +109,6 @@ void get_everything(struct task_struct* proc){
 }
 
 //Timer Callback function. This will be called when timer expires. Executes every 10 seconds until rmmod is called.
-static struct hrtimer etx_hr_timer;
-ktime_t ktime;
 enum hrtimer_restart timer_callback(struct hrtimer *timer)
 {
     /* vvv do your timer stuff here vvv */
